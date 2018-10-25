e69edb7057ee53bcfad35ed149aa19e9b0cfd84c
mrange3 <matthew.range91@gmail.com>
2018-10-25 11:16:06 -0400

    fixed rotten tomatoes

--- liri.js
+++ liri.js
@@ -58,10 +58,11 @@ function movie(details) {
     	}
 		if (!error && response.statusCode === 200) {
 
+
 		    console.log("Title: " + JSON.parse(body).Title);
 		    console.log("Release Year: " + JSON.parse(body).Year);
 		    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-		    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+		    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
 		    console.log("Country: " + JSON.parse(body).Country);
 		    console.log("Language: " + JSON.parse(body).Language);
 		    console.log("Plot: " + JSON.parse(body).Plot);
